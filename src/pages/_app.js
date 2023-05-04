bcb61df71199636c2a96babbc2b153d184344e9f
raymone970 <alclswhsworka970@gmail.com>
2023-05-03 20:56:57 -0700

    Image/ 	public/font/ 	src/Components/ 	src/pages/about.js 	src/pages/learning.js 	src/pages/projects/ 	src/styles/thebob.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import '@/styles/globals.css'
+import '@/styles/thebob.css'
+import Satoshi from '/public/font/css/satoshi.css'
+import Sentient from '/public/font/css/sentient.css'
+
+const satoshi = Satoshi;
+const sentient = Sentient;
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
