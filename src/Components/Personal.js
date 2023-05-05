284c65d5dfd47cf2dd2df536888e1f206ed463d6
konst77 <rayyoung.moon@gmail.com>
2023-05-05 10:46:14 -0700

    modified:   src/Components/Personal.js 	modified:   src/Components/about/Abtlast.js

--- src/Components/Personal.js
+++ src/Components/Personal.js
@@ -9,7 +9,7 @@ const Personal = () => {
 
                 <div className='personalmini'>
                     <h1 className='personalh'>I am a diligent, self-motivated, and design thinking minded product designer to help craft smooth experiences for digital products.</h1>
-                    <button className='herobutton'>More About Ray →</button>
+                    <button className='herobutton' href='/about'>More About Ray →</button>
                 </div>
             </div>
         </div>
--- src/Components/about/Abtlast.js
+++ src/Components/about/Abtlast.js
@@ -5,7 +5,7 @@ const Abtlast = () => {
         <div id="abtlast" className='grids'>
             <div className='abtherodiv'>
                 <h1 id='abthero' className='heroh1'>I am a diligent, self-motivated, and design thinking minded product designer to help design smooth experiences for humans.</h1>
-                <button className='herobutton' href="/learning">Ray's Learning →</button>
+                <button className='herobutton' href='/learning'>Ray's Learning →</button>
             </div>
         </div>
     )
