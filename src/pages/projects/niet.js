472b3041437a7bdb805da62f18db7ead19b93d58
konst77 <rayyoung.moon@gmail.com>
2023-05-05 10:29:13 -0700

    modified:   src/pages/projects/niet.js

--- src/pages/projects/niet.js
+++ src/pages/projects/niet.js
@@ -1,20 +1,10 @@
-import Libhero from '../Components/library/Libhero'
-import Footer from '../Components/Footer'
-import Navbar from '../Components/Navbar'
-import Shelf from '../Components/library/Shelf'
+import Footer from '../../Components/Footer'
+import Navbar from '../../Components/Navbar'
 
-export default function Library() {
+export default function Niet() {
     return (
         <>
             <Navbar />
-
-            <Libhero />
-
-            <hr></hr>
-
-            <Shelf />
-
-            <hr></hr>
 
             <Footer />
         </>
