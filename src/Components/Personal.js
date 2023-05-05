fa7d5c63b5e6528a876b55f5e5cc1991f8505675
konst77 <rayyoung.moon@gmail.com>
2023-05-04 23:57:10 -0700

    modified:   package.json 	modified:   src/Components/Personal.js

--- src/Components/Personal.js
+++ src/Components/Personal.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import profile from '/image/profile.png'
+import profileimg from '/image/profile.png'
 
 const Personal = () => {
     return(
         <div className='personal'>
             <div className='grids'>
-                <Image className='profile' src={profile} alt="profile" />
+                <Image className='profile' src={profileimg} alt="profile" />
 
                 <div className='personalmini'>
                     <h1 className='personalh'>I am a diligent, self-motivated, and design thinking minded product designer to help craft smooth experiences for digital products.</h1>
